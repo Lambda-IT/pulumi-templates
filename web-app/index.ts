06a37a3038f27e8b2cc9b6c72f6c1b47be2a3e45
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2022-07-27 09:42:35 +0200

    Updated packages and usage

--- web-app/index.ts
+++ web-app/index.ts
@@ -1,53 +1,55 @@
-import { LambdaK8sConfiguration, LambdaK8SDeployment } from '@lambda-it/pulumi-lib';
-import { InitPulumiConfig, RegistryName } from '@lambda-it/pulumi-lib/types';
-import * as pulumi from '@pulumi/pulumi';
+import {
+  LambdaK8sConfiguration,
+  LambdaK8SDeployment,
+} from "@lambda-it/pulumi-lib";
+import { InitPulumiConfig, RegistryName } from "@lambda-it/pulumi-lib/types";
+import * as pulumi from "@pulumi/pulumi";
 
 const cfg = new pulumi.Config();
 
-// load tenant/env based configuration
-const clusterContext = new pulumi.Config('kubernetes').require('context');
-const environment = cfg.require('environment');
-const identifier = cfg.require('identifier');
-const version = cfg.require('version');
-const url = `${environment}-${identifier}.lqd.lambda-it.ch`;
+// Load configuration
+const clusterContext = new pulumi.Config("kubernetes").require("context");
+const environment = cfg.require("environment");
+const project = cfg.require("project");
+const containerImage = cfg.require("container-image");
+const version = cfg.require("version");
+const url = cfg.get("url") ?? `${project}.app.lambda-it.ch`;
+const namespace = cfg.require("namespace");
 
-// general variables for deployment
-const namespace = `lambda-quick-deployment`;
+const app = "web-app";
 
-const app = 'web-app';
-const appName = `${app}-${environment}`;
-
-// logging info
-pulumi.log.info(`ID:                  ${identifier}`);
+pulumi.log.info(`Project:             ${project}`);
+pulumi.log.info(`Container Image:     ${containerImage}`);
 pulumi.log.info(`Using cluster:       ${clusterContext}`);
 pulumi.log.info(`Using namespace:     ${namespace}`);
 pulumi.log.info(`Tag to deploy:       ${version}`);
 pulumi.log.info(`ENV to deploy        ${environment}`);
 pulumi.log.info(`Requested Url:       ${url}`);
 
-const registrySecret = cfg.requireSecret('registry-secret');
+const registrySecret = cfg.requireSecret("registry-secret");
 
 const genericConfig: InitPulumiConfig = {
-    appUrls: [url],
-    dockerImage: `registry.lambda-it.ch/lqd-containers/${identifier}:${version}`,
-    labels: {
-        project: identifier,
-        app: appName,
-        env: environment,
-    },
-    namespace,
-    port: { containerPort: 80 },
-    registrySecret: {
-        name: RegistryName.HarborCR,
-        value: registrySecret,
-    },
+  appUrls: [url],
+  dockerImage: containerImage,
+  labels: {
+    project,
+    app,
+    env: environment,
+  },
+  namespace,
+  port: { containerPort: 80 },
+  registrySecret: {
+    name: RegistryName.HarborCR,
+    value: registrySecret,
+  },
 };
 
 // Deployment APP
 const appConfiguration = new LambdaK8sConfiguration(genericConfig);
-new LambdaK8SDeployment(appConfiguration)
-    .addTraefikRoute('simple')
-    .setCertificate('letsencrypt')
-    .createNamespace()
-    .createRepository()
-    .createDeployment();
+const deployment = new LambdaK8SDeployment(appConfiguration)
+  .addTraefikRoute("simple")
+  .setCertificate("letsencrypt")
+  .createRepository();
+
+// Create namespace if necessary
+deployment.createDeployment();
